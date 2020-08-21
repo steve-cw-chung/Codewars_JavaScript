1f6e31a61d1a07212e9a4a82918f1994999f83a7
Steve Chung <steve.cw.chung@gmail.com>
2020-08-21 02:29:50 -0700

    updated problem 9

--- 9.FormatNames.js
+++ 9.FormatNames.js
@@ -36,3 +36,34 @@ function list(names){
 
 
 console.log(list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'},{name: 'Homer'},{name: 'Marge'}]));
+
+/* Solution 1
+
+function list(names){
+  return names.reduce(function(prev, current, index, array){
+    if (index === 0){
+      return current.name;
+    }
+    else if (index === array.length - 1){
+      return prev + ' & ' + current.name;
+    } 
+    else {
+      return prev + ', ' + current.name;
+    }
+  }, '');
+ }
+ */
+
+ /* Solution 2 
+
+ function list(names) {
+  var xs = names.map(p => p.name)
+  var x = xs.pop()
+  return xs.length ? xs.join(", ") + " & " + x : x || ""
+}
+*/
+
+/* Solution 3 
+
+var list = (names) =>  names.map(x => x.name).join(', ').replace(/(.*),(.*)$/, "$1 &$2")
+*/
